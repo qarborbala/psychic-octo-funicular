0d813872ee82a22a53f9fd7129f8efefeb7f1400
qarborbala <nashicina34@gmail.com>
2024-01-21 08:54:09 +0000

    Fixing mem

---
+++ 21012024_085409_mem_f32zw.sh
@@ -0,0 +1,1 @@
+Behram, an Indian thug, holds the record for most murders by a single individual. He strangled 931 people between 1790-1840 with a piece of yellow and white cloth, called a ruhmal. The most murders by a woman are 612, by Countess Erzsebet Bathory of Hungary
