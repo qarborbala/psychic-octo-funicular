8c34577eb039d2ee63d446e43f6997ce6facecc3
qarborbala <nashicina34@gmail.com>
2024-01-28 08:49:19 +0000

    Changing lobbying

---
+++ 28012024_084919_lobbying_ei0fl.sh
@@ -0,0 +1,1 @@
+In 1991, during an attempted political coup on Russian President Boris Yelstin, food supplies had dwindled down at the parliament buildings so they ordered Pizza Hut to deliver pizzas
